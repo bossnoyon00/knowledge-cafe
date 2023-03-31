25c3599c30b26e107fbac8813632daf802dc1093
bossnoyon00 <bossnoyon584@gmail.com>
2023-03-31 14:57:45 +0600

    set time

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
 const SideBar = (props) => {
-    const time = props.cardTime;
-    console.log(time);
-    let number = 0;
-    for (const book of time) {
-        number = number + book.read_time;
-    }
+    
     return (
         <div>
             <h2 className='text-center'>Spend time on read :{number}  </h2>
